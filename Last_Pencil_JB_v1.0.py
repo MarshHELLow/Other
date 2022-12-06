9f96b8cc21ae48d72f96da7beb94826d5f289e69
Vladislav Chetvergov <117739126+MarshHELLow@users.noreply.github.com>
2022-12-06 09:47:35 +0300

    Add files via upload

--- Last_Pencil_JB_v1.0.py
+++ Last_Pencil_JB_v1.0.py
@@ -1,7 +1,3 @@
-import string
-
-print("How many pencils would you like to use:")
-
 def right_count():
     global count
     count = input()
@@ -15,10 +11,6 @@ def right_count():
         print("The number of pencils should be numeric")
         right_count()
     count = int(count)
-right_count()
-
-
-print("Who will be the first (John, Jack):")
 
 def right_name():
     global name
@@ -26,8 +18,34 @@ def right_name():
     if name != "John" and name != "Jack":
         print("Choose between John and Jack")
         right_name()
-right_name()
 
+def right_count_user():
+    global count_user
+    count_user = input()
+    if count_user.isdigit() == False:
+        print("Possible values: '1', '2' or '3'")
+        right_count_user()
+    if int(count_user) != 1 and int(count_user) != 2 and int(count_user) != 3:
+        print("Possible values: '1', '2' or '3'")
+        right_count_user()
+    if int(count_user) > count:
+        print("Too many pencils were taken")
+        right_count_user()
+    count_user = int(count_user)
+
+def won():
+    global name
+    if count == 0:
+        if name == "John":
+            print(f"{name} won!")
+        else:
+            name = "Jack"
+            print(f"{name}' won!")
+
+print("How many pencils would you like to use:")
+right_count()
+print("Who will be the first (John, Jack):")
+right_name()
 print("|" * count)
 while count > 0:
     if name == "John":
@@ -36,31 +54,7 @@ while count > 0:
     else:
         print(f"{name}'s turn:")
         name = "John"
-
-    def right_count_user():
-        global count_user
-        count_user = input()
-        if count_user.isdigit() == False:
-            print("Possible values: '1', '2' or '3'")
-            right_count_user()
-        if int(count_user) != 1 and int(count_user) != 2 and int(count_user) != 3:
-            print("Possible values: '1', '2' or '3'")
-            right_count_user()
-        if int(count_user) > count:
-            print("Too many pencils were taken")
-            right_count_user()
-        count_user = int(count_user)
     right_count_user()
-
     count = count - count_user
     print("|" * count)
-
-    if count == 0:
-        if name == "John":
-            print(f"{name} won!")
-        else:
-            name = "Jack"
-            print(f"{name}' won!")
-
-
-
+    won()
