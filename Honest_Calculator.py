24e338f996f14c1776414bdd54f22f65b5d50b82
Vladislav Chetvergov <117739126+MarshHELLow@users.noreply.github.com>
2022-12-09 12:57:48 +0300

    Add files via upload

--- Honest_Calculator.py
+++ Honest_Calculator.py
@@ -1,23 +1,73 @@
-msg_0 = "Enter an equation"
-msg_1 = "Do you even know what numbers are? Stay focused!"
-msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+memory = 0
 
-def main():
-    operends = ['+', '-', '*', '/']
-    print(msg_0)
-    calc = input().split()
-    try:
-        x = float(calc[0])
-        y = float(calc[-1])
-    except ValueError:
-        print(msg_1)
-        main()
-        return
-    else:
-        if calc[1] not in operends:
-            print(msg_2)
-            main()
-            return
-        return
+def error(msg_error):
+    if msg_error == 0:
+        msg_text = "Enter an equation"
+    elif msg_error == 1:
+        msg_text = "Do you even know what numbers are? Stay focused!"
+    elif msg_error == 2:
+        msg_text = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+    elif msg_error == 3:
+        msg_text = "Yeah... division by zero. Smart move..."
+    elif msg_error == 4:
+        msg_text = "Do you want to store the result? (y / n):"
+    elif msg_error == 5:
+        msg_text = "Do you want to continue calculations? (y / n):"
+    return msg_text
 
-main()
+operends = ['+', '-', '*', '/']
+
+while True:
+    while True:
+        print(error(0))
+        calc = input().split(" ")
+        x, oper, y = calc[0], calc[1], calc[2]
+        if x == 'M':
+            x = memory
+        if y == 'M':
+            y = memory
+        try:
+            x, y = float(x), float(y)
+        except ValueError:
+            print(error(1))
+        else:
+            if oper in operends:
+                if oper == "+":
+                    result = float(x) + float(y)
+                    break
+                elif oper == "-":
+                    result = float(x) - float(y)
+                    break
+                elif oper == "*":
+                    result = float(x) * float(y)
+                    break
+                else:
+                    if (oper == "/") and (y == 0):
+                        print(error(3))
+                        continue
+                    else:
+                        result = float(x) / float(y)
+                        break
+            else:
+                print(error(2))
+    print(result)
+
+#memory
+
+    while True:
+        print(error(4))
+        answer = input()
+        if answer == 'y':
+            memory = result
+            break
+        elif answer == 'n':
+            break
+        else:
+            break
+    while True:
+        print(error(5))
+        answer = input()
+        if answer == 'y':
+            break
+        elif answer == 'n':
+            exit(0)
